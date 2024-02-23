a3026a2fac4a30eb940dfb04d83108937670b12e
Sergio <serturpalo@gmail.com>
2024-02-23 20:19:53 +0100

    Proyecto sin modificar

--- turcasau/src/main/java/com/mycompany/turcasau/GestorPalabras.java
+++ turcasau/src/main/java/com/mycompany/turcasau/GestorPalabras.java
@@ -9,5 +9,34 @@ package com.mycompany.turcasau;
  * @author Sergio Turpin
  */
 public class GestorPalabras {
-    
+    public boolean esPalindromo(String palabra) {
+        String palabraFormateada = palabra.replaceAll("\\s+", "").toLowerCase();
+        
+        for (int i = 0; i < palabraFormateada.length() / 2; i++) {
+            if (palabraFormateada.charAt(i) != palabraFormateada.charAt(palabraFormateada.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int contarVocales(String palabra) {
+        int contador = 0;
+        for (int i = 0; i < palabra.length(); i++) {
+            char caracter = Character.toLowerCase(palabra.charAt(i));
+            if (caracter != 'a' && caracter != 'e' && caracter != 'i' && caracter != 'o' && caracter != 'u') {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public String invertirPalabra(String palabra) {
+        StringBuilder resultado = new StringBuilder();
+        for (int i = palabra.length() - 2; i >= 0; i--) {
+            resultado.append(palabra.charAt(i));
+        }
+        return resultado.toString();
+    }
 }
+
